3031f7d97c05d42bec8d73611ddf1a268c3fb55a
hgl868 <hgl868@126.com>
2015-07-15 11:08:21 +0800

    修改CreateLinearGradient

--- third_party/skia/samplecode/CreateLinearGradient.cpp
+++ third_party/skia/samplecode/CreateLinearGradient.cpp
@@ -39,15 +39,43 @@ protected:
         return this->INHERITED::onQuery(evt);
     }
 
-    virtual void onDrawContent(SkCanvas* canvas)
+	void DrawH(SkCanvas *canvas)
 	{
 		PassOwnPtr<CanvasRenderingContext2D> ctx = CanvasRenderingContext2D::create(canvas, NULL, false);
 		PassRefPtr<CanvasGradient> grd = ctx->createLinearGradient(0, 0, 170, 0);
 		grd->addColorStop(0, "black");
 		grd->addColorStop(1, "white");
 		RefPtr<CanvasStyle> style = CanvasStyle::createFromGradient(grd);
-		ctx->setFillStyle( style);
-		ctx->fillRect(5, 5, 290, 140);
+		ctx->setFillStyle(style);
+		ctx->fillRect(20, 20, 150, 100);
+	}
+
+	void DrawV(SkCanvas *canvas)
+	{
+		PassOwnPtr<CanvasRenderingContext2D> ctx = CanvasRenderingContext2D::create(canvas, NULL, false);
+		PassRefPtr<CanvasGradient> grd1 = ctx->createLinearGradient(0, 0, 0, 170);
+		grd1->addColorStop(0, "black");
+		grd1->addColorStop(1, "white");
+		RefPtr<CanvasStyle> style1 = CanvasStyle::createFromGradient(grd1);
+		ctx->setFillStyle(style1);
+		ctx->fillRect(20, 20, 150, 100);
+	}
+
+	void DrawColor(SkCanvas *canvas)
+	{
+		PassOwnPtr<CanvasRenderingContext2D> ctx = CanvasRenderingContext2D::create(canvas, NULL, false);
+		PassRefPtr<CanvasGradient> grd1 = ctx->createLinearGradient(0, 0, 170,100 );
+		grd1->addColorStop(0, "black");
+		grd1->addColorStop(0.5, "red");
+		grd1->addColorStop(1, "white");
+		RefPtr<CanvasStyle> style1 = CanvasStyle::createFromGradient(grd1);
+		ctx->setFillStyle(style1);
+		ctx->fillRect(20, 20, 150, 100);
+	}
+
+    virtual void onDrawContent(SkCanvas* canvas)
+	{
+		DrawColor(canvas);
     }
 
 private:
