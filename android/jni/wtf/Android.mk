cb93861bca567e239641c241ec4d152b1c52b32a
hgl868 <houguoli@egret.com>
2015-07-29 00:29:47 +0800

    添加wtf mk 文件

---
+++ android/jni/wtf/Android.mk
@@ -0,0 +1,47 @@
+LOCAL_PATH := $(call my-dir)
+
+
+include $(CLEAR_VARS)
+
+LOCAL_ARM_MODE := arm
+
+LOCAL_MODULE := wtf
+
+LOCAL_CFLAGS += -DHAVE_OT \
+				-DGL_GLEXT_PROTOTYPES \
+				-DHB_NO_UNICODE_FUNCS  \
+				-DHB_NO_MT \
+				-fshort-wchar	\
+				-fexceptions
+				
+LOCAL_C_INCLUDES :=  \
+					$(JNI_H_INCLUDE)  \
+					$(MY_ANDROID_SOURCE)/frameworks/base/include \
+					$(MY_ANDROID_SOURCE)/system/core/include \
+					$(MY_ANDROID_SOURCE)/external/astl/include \
+					$(MY_ANDROID_SOURCE)/hardware/libhardware/include \
+					$(LOCAL_PATH)      \
+					$../../wtf/dtoa \
+					$../../wtf \
+					$../../ \		
+
+#harfbuzz
+LOCAL_SRC_FILES	+=  ../../../wtf/dtoa/bignum.cc \
+					../../../wtf/dtoa/bignum-dtoa.cc \
+					../../../wtf/dtoa/cached-powers.cc \
+					../../../wtf/dtoa/diy-fp.cc \
+					../../../wtf/dtoa/double-conversion.cc \
+					../../../wtf/dtoa/fast-dtoa.cc \
+					../../../wtf/dtoa/fixed-dtoa.cc \
+					../../../wtf/dtoa/strtod.cc \
+				
+					
+
+				
+
+
+#LOCAL_LDLIBS := $(MY_LIBRARY)
+
+include $(BUILD_STATIC_LIBRARY)
+
+
