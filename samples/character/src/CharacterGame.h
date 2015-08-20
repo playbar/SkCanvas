dc5b656970a2d33ed3f28114f83168010f8d639b
hgl868 <hgl868@126.com>
2015-08-20 11:15:42 +0800

    修改绘制代码

--- samples/character/src/CharacterGame.h
+++ samples/character/src/CharacterGame.h
@@ -74,48 +74,14 @@ protected:
 private:
     
     bool visitInitNode(Node* node);
-    void initializeMaterial(Scene* scene, Node* node, Material* material);
     void initializeCharacter();
     void drawSplash(void* param);
 	bool visitDrawNode(Node* node, void *cookie);
     void play(const char* id, bool repeat, float speed = 1.0f);
-    void jump();
-    void kick();
-    void adjustCamera(float elapsedTime);
-    bool isOnFloor() const;
-    void clone();
-    void grabBall();
-    void releaseBall();
 
 	GrContext *fCurContext;
 	GrRenderTarget *fCurRenderTarget;
 	SkCanvas * fCanvas;
-
-    Font* _font;
-    Scene* _scene;
-    PhysicsCharacter* _character;
-    Node* _characterNode;
-    Node* _characterMeshNode;
-    Node* _characterShadowNode;
-    Node* _basketballNode;
-    float _floorLevel;
-    Animation* _animation;
-    AnimationClip* _currentClip;
-    AnimationClip* _jumpClip;
-    AnimationClip* _kickClip;
-    int _rotateX;
-    MaterialParameter* _materialParameterAlpha;
-    unsigned int _keyFlags;
-    bool _physicsDebug;
-    bool _wireframe;
-    Vector3 _oldBallPosition;
-    bool _hasBall;
-    bool _applyKick;
-    bool _kicking;
-    float _kickDelay;
-    bool* _buttonPressed;
-    Vector2 _currentDirection;
-    Gamepad* _gamepad;
 
 };
 
