73cccf0b661c4acbdb30927093e3d5544ecedfe0
hgl868 <hgl868@126.com>
2015-08-25 15:07:34 +0800

    添加接口实现函数

--- CanvasContext/v8binding/CanvasContext2D_v8.cpp
+++ CanvasContext/v8binding/CanvasContext2D_v8.cpp
@@ -95,22 +95,34 @@ void v8_CanvasContext2D_createRadialGradient(const v8::FunctionCallbackInfo<v8::
 
 void v8_CanvasContext2D_setLineDash(const FunctionCallbackInfo<Value> &args)
 {
-
+	//todo
 }
 
 void v8_CanvasContext2D_getLineDash(const FunctionCallbackInfo<Value> &args)
 {
-
+	//todo
 }
 
 void v8_CanvasContext2D_clearRect(const FunctionCallbackInfo<Value> &args)
 {
-
+	BEGIN_SCOPE_WHITH_ARGS(4);
+	CanvasContext2D *imp = UnwrapCanvasContext2D(args.Holder());
+	float x = static_cast<float>(args[0]->NumberValue());
+	float y = static_cast<float>(args[1]->NumberValue());
+	float w = static_cast<float>(args[2]->NumberValue());
+	float h = static_cast<float>(args[3]->NumberValue());
+	imp->clearRect(x, y, w, h);
 }
 
 void v8_CanvasContext2D_fillRect(const FunctionCallbackInfo<Value> &args)
 {
-
+	BEGIN_SCOPE_WHITH_ARGS(4);
+	CanvasContext2D *imp = UnwrapCanvasContext2D(args.Holder());
+	float x = static_cast<float>(args[0]->NumberValue());
+	float y = static_cast<float>(args[1]->NumberValue());
+	float w = static_cast<float>(args[2]->NumberValue());
+	float h = static_cast<float>(args[3]->NumberValue());
+	imp->fillRect(x, y, w, h);
 }
 
 void v8_CanvasContext2D_beginPath(const FunctionCallbackInfo<Value> &args)
@@ -121,7 +133,8 @@ void v8_CanvasContext2D_beginPath(const FunctionCallbackInfo<Value> &args)
 
 void v8_CanvasContext2D_closePath(const FunctionCallbackInfo<Value> &args)
 {
-
+	CanvasContext2D *imp = UnwrapCanvasContext2D(args.Holder());
+	imp->closePath();
 }
 
 void v8_CanvasContext2D_moveTo(const FunctionCallbackInfo<Value> &args)
@@ -144,37 +157,83 @@ void v8_CanvasContext2D_lineTo(const FunctionCallbackInfo<Value> &args)
 
 void v8_CanvasContext2D_quadraticCurveTo(const FunctionCallbackInfo<Value> &args)
 {
-
+	BEGIN_SCOPE_WHITH_ARGS(4);
+	CanvasContext2D *imp = UnwrapCanvasContext2D(args.Holder());
+	float cpx = static_cast<float>(args[0]->NumberValue());
+	float cpy = static_cast<float>(args[1]->NumberValue());
+	float x = static_cast<float>(args[2]->NumberValue());
+	float y = static_cast<float>(args[3]->NumberValue());
+	imp->quadraticCurveTo(cpx, cpy, x, y);
 }
 
 void v8_CanvasContext2D_bezierCurveTo(const FunctionCallbackInfo<Value> &args)
 {
-
+	BEGIN_SCOPE_WHITH_ARGS(6);
+	CanvasContext2D *imp = UnwrapCanvasContext2D(args.Holder());
+	float cp1x = static_cast<float>(args[0]->NumberValue());
+	float cp1y = static_cast<float>(args[1]->NumberValue());
+	float cp2x = static_cast<float>(args[2]->NumberValue());
+	float cp2y = static_cast<float>(args[3]->NumberValue());
+	float x = static_cast<float>(args[4]->NumberValue());
+	float y = static_cast<float>(args[5]->NumberValue());
+	imp->bezierCurveTo(cp1x, cp1y, cp2x, cp2y, x, y);
 }
 
 void v8_CanvasContext2D_arcTo(const FunctionCallbackInfo<Value> &args)
 {
-
+	BEGIN_SCOPE_WHITH_ARGS(5);
+	CanvasContext2D *imp = UnwrapCanvasContext2D(args.Holder());
+	float x1 = static_cast<float>(args[0]->NumberValue());
+	float y1 = static_cast<float>(args[1]->NumberValue());
+	float x2 = static_cast<float>(args[2]->NumberValue());
+	float y2 = static_cast<float>(args[3]->NumberValue());
+	float radius = static_cast<float>(args[4]->NumberValue());
+	imp->arcTo(x1, y1, x2, y2, radius);
 }
 
 void v8_CanvasContext2D_rect(const FunctionCallbackInfo<Value> &args)
 {
-
+	BEGIN_SCOPE_WHITH_ARGS(4);
+	CanvasContext2D *imp = UnwrapCanvasContext2D(args.Holder());
+	float x = static_cast<float>(args[0]->NumberValue());
+	float y = static_cast<float>(args[1]->NumberValue());
+	float width = static_cast<float>(args[2]->NumberValue());
+	float height = static_cast<float>(args[3]->NumberValue());
+	imp->rect(x, y, width, height);
 }
 
 void v8_CanvasContext2D_arc(const FunctionCallbackInfo<Value> &args)
 {
-
+	BEGIN_SCOPE_WHITH_ARGS(5);
+	CanvasContext2D *imp = UnwrapCanvasContext2D(args.Holder());
+	float x = static_cast<float>(args[0]->NumberValue());
+	float y = static_cast<float>(args[1]->NumberValue());
+	float radius = static_cast<float>(args[2]->NumberValue());
+	float startAngle = static_cast<float>(args[3]->NumberValue());
+	float endAngle = static_cast<float>(args[4]->NumberValue());
+	bool anticlockwise = args[5]->BooleanValue();
+	imp->arc(x, y, radius, startAngle, endAngle, anticlockwise);
 }
 
 void v8_CanvasContext2D_ellipse(const FunctionCallbackInfo<Value> &args)
 {
-
+	BEGIN_SCOPE_WHITH_ARGS(7);
+	CanvasContext2D *imp = UnwrapCanvasContext2D(args.Holder());
+	float x = static_cast<float>(args[0]->NumberValue());
+	float y = static_cast<float>(args[1]->NumberValue());
+	float radiusX = static_cast<float>(args[2]->NumberValue());
+	float radiusY = static_cast<float>(args[3]->NumberValue());
+	float rotation = static_cast<float>(args[4]->NumberValue());
+	float startAngle = static_cast<float>(args[5]->NumberValue());
+	float endAngle = static_cast<float>(args[6]->NumberValue());
+	bool anticlockwise = args[7]->BooleanValue();
+	imp->ellipse(x, y, radiusX, radiusY, rotation, startAngle, endAngle, anticlockwise);
 }
 
 void v8_CanvasContext2D_fill(const FunctionCallbackInfo<Value> &args)
 {
-
+	CanvasContext2D *imp = UnwrapCanvasContext2D(args.Holder());
+	imp->fill();
 }
 
 void v8_CanvasContext2D_stroke(const FunctionCallbackInfo<Value> &args)
@@ -185,32 +244,48 @@ void v8_CanvasContext2D_stroke(const FunctionCallbackInfo<Value> &args)
 
 void v8_CanvasContext2D_clip(const FunctionCallbackInfo<Value> &args)
 {
-
+	CanvasContext2D *imp = UnwrapCanvasContext2D(args.Holder());
+	imp->clip();
 }
 
 void v8_CanvasContext2D_isPointInPath(const FunctionCallbackInfo<Value> &args)
 {
-
+	BEGIN_SCOPE_WHITH_ARGS(2);
+	CanvasContext2D *imp = UnwrapCanvasContext2D(args.Holder());
+	float x = static_cast<float>(args[0]->NumberValue());
+	float y = static_cast<float>(args[1]->NumberValue());
+	args.GetReturnValue().Set(imp->isPointInPath(x, y));
 }
 
 void v8_CanvasContext2D_isPointInStroke(const FunctionCallbackInfo<Value> &args)
 {
-
+	//todo
 }
 
 void v8_CanvasContext2D_measureText(const FunctionCallbackInfo<Value> &args)
 {
-
+	BEGIN_SCOPE_WHITH_ARGS(1);
+	CanvasContext2D *imp = UnwrapCanvasContext2D(args.Holder());
+	v8::String::Utf8Value str(args[0]);
+	const char* cstr = ToCString(str);
+	imp->measureText(cstr);
 }
 
 void v8_CanvasContext2D_setAlpha(const FunctionCallbackInfo<Value> &args)
 {
-
+	BEGIN_SCOPE_WHITH_ARGS(1);
+	CanvasContext2D *imp = UnwrapCanvasContext2D(args.Holder());
+	float alpha = static_cast<float>(args[0]->NumberValue());
+	imp->setGlobalAlpha(alpha);
 }
 
 void v8_CanvasContext2D_setCompositeOperation(const FunctionCallbackInfo<Value> &args)
 {
-
+	BEGIN_SCOPE_WHITH_ARGS(1);
+	CanvasContext2D *imp = UnwrapCanvasContext2D(args.Holder());
+	v8::String::Utf8Value str(args[0]);
+	const char* cstr = ToCString(str);
+	imp->setGlobalCompositeOperation(cstr);
 }
 
 void v8_CanvasContext2D_setLineWidth(const FunctionCallbackInfo<Value> &args)
@@ -223,42 +298,71 @@ void v8_CanvasContext2D_setLineWidth(const FunctionCallbackInfo<Value> &args)
 
 void v8_CanvasContext2D_setLineCap(const FunctionCallbackInfo<Value> &args)
 {
-
+	BEGIN_SCOPE_WHITH_ARGS(1);
+	CanvasContext2D *imp = UnwrapCanvasContext2D(args.Holder());
+	v8::String::Utf8Value str(args[0]);
+	const char* cstr = ToCString(str);
+	imp->setLineCap(cstr);
 }
 
 void v8_CanvasContext2D_setLineJoin(const FunctionCallbackInfo<Value> &args)
 {
-
+	BEGIN_SCOPE_WHITH_ARGS(1);
+	CanvasContext2D *imp = UnwrapCanvasContext2D(args.Holder());
+	v8::String::Utf8Value str(args[0]);
+	const char* cstr = ToCString(str);
+	imp->setLineJoin(cstr);
 }
 
 void v8_CanvasContext2D_setMiterLimit(const FunctionCallbackInfo<Value> &args)
 {
-
+	BEGIN_SCOPE_WHITH_ARGS(1);
+	CanvasContext2D *imp = UnwrapCanvasContext2D(args.Holder());
+	float limit = static_cast<float>(args[0]->NumberValue());
+	imp->setMiterLimit(limit);
 }
 
 void v8_CanvasContext2D_clearShadow(const FunctionCallbackInfo<Value> &args)
 {
-
+	//todo
 }
 
 void v8_CanvasContext2D_fillText(const FunctionCallbackInfo<Value> &args)
 {
-
+	BEGIN_SCOPE_WHITH_ARGS(3);
+	CanvasContext2D *imp = UnwrapCanvasContext2D(args.Holder());
+	v8::String::Utf8Value str(args[0]);
+	const char* cstr = ToCString(str);
+	float x = static_cast<float>(args[1]->NumberValue());
+	float y = static_cast<float>(args[2]->NumberValue());
+	imp->fillText(cstr, x, y);
 }
 
 void v8_CanvasContext2D_strokeText(const FunctionCallbackInfo<Value> &args)
 {
-
+	BEGIN_SCOPE_WHITH_ARGS(3);
+	CanvasContext2D *imp = UnwrapCanvasContext2D(args.Holder());
+	v8::String::Utf8Value str(args[0]);
+	const char* cstr = ToCString(str);
+	float x = static_cast<float>(args[1]->NumberValue());
+	float y = static_cast<float>(args[2]->NumberValue());
+	imp->strokeText(cstr, x, y);
 }
 
 void v8_CanvasContext2D_setStrokeColor(const FunctionCallbackInfo<Value> &args)
 {
-
+	//todo
 }
 
 void v8_CanvasContext2D_strokeRect(const FunctionCallbackInfo<Value> &args)
 {
-
+	BEGIN_SCOPE_WHITH_ARGS(3);
+	CanvasContext2D *imp = UnwrapCanvasContext2D(args.Holder());
+	float x = static_cast<float>(args[0]->NumberValue());
+	float y = static_cast<float>(args[1]->NumberValue());
+	float w = static_cast<float>(args[2]->NumberValue());
+	float h = static_cast<float>(args[3]->NumberValue());
+	imp->strokeRect(x, y, w, h);
 }
 
 void v8_CanvasContext2D_drawImage(const FunctionCallbackInfo<Value> &args)
@@ -358,7 +462,6 @@ Handle<FunctionTemplate> CanvasContext2D_Class(Isolate *isolate )
 	temp_proto->Set(String::NewFromUtf8(isolate, "createPattern", v8::NewStringType::kNormal).ToLocalChecked(), v8::FunctionTemplate::New(isolate, v8_CanvasContext2D_createPattern));
 	temp_proto->Set(String::NewFromUtf8(isolate, "createImageData", v8::NewStringType::kNormal).ToLocalChecked(), v8::FunctionTemplate::New(isolate, v8_CanvasContext2D_createImageData));
 	temp_proto->Set(String::NewFromUtf8(isolate, "getImageData", v8::NewStringType::kNormal).ToLocalChecked(), v8::FunctionTemplate::New(isolate, v8_CanvasContext2D_getImageData));
-
 
 	Handle<ObjectTemplate> temp_inst = temp_class->InstanceTemplate();
 	temp_inst->SetInternalFieldCount(1);
