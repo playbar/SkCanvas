1effb93cbd14b6fccc8d4c249a7ac2741de85ced
hgl868 <hgl868@126.com>
2015-08-31 22:41:14 +0800

    修改android 平台编译错误

--- CanvasContext/utils/HexNumber.h
+++ CanvasContext/utils/HexNumber.h
@@ -1,6 +1,7 @@
 #ifndef HexNumber_h
 #define HexNumber_h
 
+#include <vector>
 
 namespace Canvas2D {
 
@@ -45,37 +46,37 @@ inline void placeByteAsHex(unsigned char byte, T& destination, HexConversionMode
     *destination++ = hexDigits[byte & 0xF];
 }
 
-template<typename T>
-inline void appendUnsignedAsHex(unsigned number, T& destination, HexConversionMode mode = Uppercase)
-{
-	const char* hexDigits = Internal::hexDigitsForMode(mode);
-	std::vector<char> result;
-	result.resize(8);
-    do {
-        result.prepend(hexDigits[number % 16]);
-        number >>= 4;
-    } while (number > 0);
-
-    destination.append(result.data(), result.size());
-}
+//template<typename T>
+//inline void appendUnsignedAsHex(unsigned number, T& destination, HexConversionMode mode = Uppercase)
+//{
+//	const char* hexDigits = Internal::hexDigitsForMode(mode);
+//	std::vector<char> result;
+//	result.resize(8);
+//    do {
+//        result.push_back(hexDigits[number % 16]);
+//        number >>= 4;
+//    } while (number > 0);
+//
+//    destination.append(result.data(), result.size());
+//}
 
 // Same as appendUnsignedAsHex, but using exactly 'desiredDigits' for the conversion.
-template<typename T>
-inline void appendUnsignedAsHexFixedSize(unsigned number, T& destination, unsigned desiredDigits, HexConversionMode mode = Uppercase)
-{
-    ASSERT(desiredDigits);
-
-	const char* hexDigits = Internal::hexDigitsForMode(mode);
-	std::vector<char> result;
-	result.resize(8);
-    do {
-        result.prepend(hexDigits[number % 16]);
-        number >>= 4;
-    } while (result.size() < desiredDigits);
-
-    ASSERT(result.size() == desiredDigits);
-    destination.append(result.data(), result.size());
-}
+//template<typename T>
+//inline void appendUnsignedAsHexFixedSize(unsigned number, T& destination, unsigned desiredDigits, HexConversionMode mode = Uppercase)
+//{
+//    //ASSERT(desiredDigits);
+//
+//	const char* hexDigits = Internal::hexDigitsForMode(mode);
+//	std::vector<char> result;
+//	result.resize(8);
+//    do {
+//        result.prepend(hexDigits[number % 16]);
+//        number >>= 4;
+//    } while (result.size() < desiredDigits);
+//
+//    ASSERT(result.size() == desiredDigits);
+//    destination.append(result.data(), result.size());
+//}
 
 } // 
 
